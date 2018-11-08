e39b3949f951980bb1ddd7c2b467ba8c5faf2e8a
Bryan Arbelo <magestican.visualkei@gmail.com>
2018-11-09 10:52:18 +1100

    changed styling of faq page as per request in test

--- client/app/components/pages/faq/index.js
+++ client/app/components/pages/faq/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getFaqPageData } from './../../../actions/faqPage';
-import { Card, Icon } from 'semantic-ui-react'
+import { Card, Button, Divider, Grid } from 'semantic-ui-react'
 
 
 @connect(mapStateToProps)
 export default class FaqPage extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selectedFaqId: 0,
+    }
+  }
+
+  selectFaq = (faqId) => {
+    this.setState({ selectedFaqId: faqId });
   }
 
   componentDidMount = () => {
@@ -17,22 +25,30 @@ export default class FaqPage extends Component {
 
   render() {
     const { faqPageData: { faqs } } = this.props;
-    console.log(faqs);
+    const { selectedFaqId } = this.state;
+    const selectedFaq = faqs[selectedFaqId] || {};
     return (
       <div className="faq-page">
-        <Card.Group>
-          {
-            faqs.map((o, i) => {
-              return <Card color='red' key={`faq${i}`}>
+        <Grid columns={2} stackable textAlign='center'>
+          <Grid.Row verticalAlign='middle'>
+            <Grid.Column className="left-panel">
+              <Card color='red'>
                 <Card.Content>
-                  <Card.Header content={o.title} />
-                  <Card.Meta content={`Question ${i}`} />
-                  <Card.Description dangerouslySetInnerHTML={{ __html: o.body }} />
+                  <Card.Header content={selectedFaq.title} />
+                  <Card.Meta content={`Question ${selectedFaqId}`} />
+                  <Card.Description dangerouslySetInnerHTML={{ __html: selectedFaq.body }} />
                 </Card.Content>
               </Card>
-            })
-          }
-        </Card.Group>
+            </Grid.Column>
+            <Grid.Column className="right-panel">
+              {
+                faqs.map((o, i) => {
+                  return <Button onClick={this.selectFaq.bind(this,i)} primary key={`faq${i}`}>{o.title}</Button>
+                })
+              }
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     )
   }
